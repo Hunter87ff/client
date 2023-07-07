f3a0a1d658b022d596bc7be311843642f9725d11
hunter87ff <huntergaming702@gmail.com>
2023-07-08 00:12:00 +0530

    update

--- propaco/static/js/script.js
+++ propaco/static/js/script.js
@@ -19,3 +19,16 @@ function toggleMenu() {
     menuButton.classList.toggle('menu-open');
     menu.classList.toggle("show")
 }
+
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+    })
+})
+const hiddenelm = document.querySelectorAll(".type-con");
+hiddenelm.forEach((el) => observer.observe(el));
